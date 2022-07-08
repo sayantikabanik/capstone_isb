13c72e40b082b4ab21e44834d3095d5688fd0a85
sayantikabanik <sayantikabanik122@gmail.com>
2022-07-08 17:49:54 +0530

    WIP- pipeline updated

--- analysis_framework/pipeline/process.py
+++ analysis_framework/pipeline/process.py
@@ -1,10 +1,14 @@
 import intake
-from dagster import asset
+import pandas as pd
+
+from dagster import get_dagster_logger, job, op
 from analysis_framework import DATA_CATALOG_PATH
-from dagster import materialize
+from factor_analyzer import FactorAnalyzer
+
+logger = get_dagster_logger()
 
 
-@asset
+@op
 def read_raw_data():
     catalog_path = DATA_CATALOG_PATH.joinpath("catalog.yml")
     catalog = intake.open_catalog(catalog_path)
@@ -13,11 +17,39 @@ def read_raw_data():
     return df_raw
 
 
-#TODO: Add pre-processing and text score logic
-@asset
-def process_raw_data(read_raw_data):
+@op
+def subset_data(read_raw_data):
+    df_subset = read_raw_data[['skillDevelopment', 'salaryBenefits',
+               'workLifeBalance', 'workSatisfaction',
+               'jobSecurity', 'careerGrowth', 'companyCulture']]
+
+    df_subset = df_subset.dropna()
+    df_reset = df_subset.reset_index(drop=True)
+    logger.info("data shape",df_reset.shape)
+    return df_reset
+
+
+@op
+def factor_analysis(df_reset):
+    fa = FactorAnalyzer(n_factors=2, rotation='varimax')
+    fa.fit(df_reset)
+    df_factor_scores = pd.DataFrame(fa.transform(df_reset))
+    df_reset[['factor1', 'factor2']] = df_factor_scores
+    fa_data = df_reset
+    logger.info("Data for factor analysis computation", fa_data.head())
     return 0
 
+@job
+def compute():
+    df_raw = read_raw_data()
+    df_subset = subset_data(df_raw)
+    df_result = factor_analysis(df_subset)
+    # factor_df = df_result[['factor1', 'factor2']]
+    print(df_result.head())
+    logger.info("Factor compute results", df_result)
+    # logger.info("Min and max for factors computed", factor_df.min(), factor_df.max())
+    #
 
-if __name__ == "__main__":
-    materialize([read_raw_data, process_raw_data])
+# @asset
+# def cluster_analysis():
+#     factor_analysis(df_reset)
