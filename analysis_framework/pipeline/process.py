7a7d3cd785fc88148c84540cdcaba2bfb3caa345
sayantikabanik <sayantikabanik122@gmail.com>
2022-07-10 00:02:21 +0530

    pipeline updates and env additions

--- analysis_framework/pipeline/process.py
+++ analysis_framework/pipeline/process.py
@@ -1,11 +1,17 @@
 import intake
 import pandas as pd
 
-from dagster import get_dagster_logger, job, op
+from dagster import get_dagster_logger, job, op, Output, graph
 from analysis_framework import DATA_CATALOG_PATH
 from factor_analyzer import FactorAnalyzer
 
 logger = get_dagster_logger()
+
+#
+# @op
+# def index_reset(df):
+#     df_reset = df.reset_index(drop=True)
+#     return df_reset
 
 
 @op
@@ -13,31 +19,28 @@ def read_raw_data():
     catalog_path = DATA_CATALOG_PATH.joinpath("catalog.yml")
     catalog = intake.open_catalog(catalog_path)
     df_raw = catalog.review_dataset.read()
-    print(df_raw.head())
     return df_raw
 
 
 @op
-def subset_data(read_raw_data):
-    df_subset = read_raw_data[['skillDevelopment', 'salaryBenefits',
-               'workLifeBalance', 'workSatisfaction',
-               'jobSecurity', 'careerGrowth', 'companyCulture']]
-
+def subset_data(df_raw):
+    df_subset = df_raw[['skillDevelopment', 'salaryBenefits',
+                        'workLifeBalance', 'workSatisfaction',
+                        'jobSecurity', 'careerGrowth', 'companyCulture']]
     df_subset = df_subset.dropna()
-    df_reset = df_subset.reset_index(drop=True)
-    logger.info("data shape",df_reset.shape)
-    return df_reset
+    return df_subset
 
 
 @op
-def factor_analysis(df_reset):
+def factor_analysis(df_subset):
+    df_reset = df_subset.reset_index(drop=True)
     fa = FactorAnalyzer(n_factors=2, rotation='varimax')
     fa.fit(df_reset)
     df_factor_scores = pd.DataFrame(fa.transform(df_reset))
-    df_reset[['factor1', 'factor2']] = df_factor_scores
+    df_subset[['factor1', 'factor2']] = df_factor_scores
     fa_data = df_reset
-    logger.info("Data for factor analysis computation", fa_data.head())
-    return 0
+    return fa_data
+
 
 @job
 def compute():
@@ -45,11 +48,8 @@ def compute():
     df_subset = subset_data(df_raw)
     df_result = factor_analysis(df_subset)
     # factor_df = df_result[['factor1', 'factor2']]
-    print(df_result.head())
-    logger.info("Factor compute results", df_result)
-    # logger.info("Min and max for factors computed", factor_df.min(), factor_df.max())
-    #
+    # logger.info("Factor compute results", factor_df.head())
 
-# @asset
-# def cluster_analysis():
-#     factor_analysis(df_reset)
+
+if __name__ == "__main__":
+    result = compute.execute_in_process()
