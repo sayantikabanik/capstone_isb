2caf1068b98e89165573d6fec2387cd097ac2592
sayantikabanik <sayantikabanik122@gmail.com>
2022-07-10 22:50:04 +0530

    pipeline updated

--- analysis_framework/pipeline/process.py
+++ analysis_framework/pipeline/process.py
@@ -5,13 +5,8 @@ from dagster import get_dagster_logger, job, op, Output, graph
 from analysis_framework import DATA_CATALOG_PATH
 from factor_analyzer import FactorAnalyzer
 
-logger = get_dagster_logger()
-
-#
-# @op
-# def index_reset(df):
-#     df_reset = df.reset_index(drop=True)
-#     return df_reset
+from sklearn.preprocessing import normalize
+from sklearn.cluster import KMeans
 
 
 @op
@@ -32,23 +27,69 @@ def subset_data(df_raw):
 
 
 @op
-def factor_analysis(df_subset):
-    df_reset = df_subset.reset_index(drop=True)
+def index_reset(df):
+    # important step: for factor analysis to have a similar sl no of the data
+    df_reset = df.reset_index(drop=True)
+    return df_reset
+
+
+@op
+def factor_analysis(df_subset_fa) -> Output:
     fa = FactorAnalyzer(n_factors=2, rotation='varimax')
-    fa.fit(df_reset)
-    df_factor_scores = pd.DataFrame(fa.transform(df_reset))
-    df_subset[['factor1', 'factor2']] = df_factor_scores
-    fa_data = df_reset
+    fa.fit(df_subset_fa)
+    df_factor_scores = pd.DataFrame(fa.transform(df_subset_fa))
+    df_subset_fa[['factor1', 'factor2']] = df_factor_scores
+    fa_data = df_subset_fa
+    print(fa_data)
     return fa_data
+
+
+@op
+def write_csv(df_result):
+    df_result.to_csv('cluster_output.csv')
+    return 0
+
+
+@op
+def intermediate_clustering_step(df_result):
+    factor_df = df_result[['factor1', 'factor2']]
+    return factor_df
+
+
+@op
+def scaling_data(factor_df):
+    data_scaled = normalize(factor_df)
+    data_scaled = pd.DataFrame(data_scaled, columns=factor_df.columns)
+    return data_scaled
+
+
+@op
+def clustering_processed_factor_data(scaled_df, df_result):
+    kmeans = KMeans(init="k-means++", n_clusters=5, n_init=10,   max_iter=300,random_state=42)
+    kmeans.fit(scaled_df)
+    kmeans.inertia_
+    kmeans.cluster_centers_
+    df_cluster = df_result
+    df_cluster['cluster'] = kmeans.labels_
+    return df_cluster
+
+
+@op
+def cluster_operations(cluster_df):
+    return cluster_df.groupby('cluster').mean()
 
 
 @job
 def compute():
     df_raw = read_raw_data()
     df_subset = subset_data(df_raw)
-    df_result = factor_analysis(df_subset)
-    # factor_df = df_result[['factor1', 'factor2']]
-    # logger.info("Factor compute results", factor_df.head())
+    df_fa = index_reset(df_subset)
+    df_result = factor_analysis(df_fa)
+    factor_df = intermediate_clustering_step(df_result)
+    scaled_df = scaling_data(factor_df)
+    cluster_df = clustering_processed_factor_data(scaled_df, df_result)
+    cluster_operation_df = cluster_operations(cluster_df)
+    write_csv(cluster_df)
 
 
 if __name__ == "__main__":
