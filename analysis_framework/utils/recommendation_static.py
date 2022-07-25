1a6ca0fe32d0f4e2802ad48f26db6b338d98d263
Suprateek <103513253+suprateekp@users.noreply.github.com>
2022-07-26 00:51:40 +0530

    Update recommendation_static.py

--- analysis_framework/utils/recommendation_static.py
+++ analysis_framework/utils/recommendation_static.py
@@ -2,6 +2,6 @@
 
 recommend = {
              "medium_amber": {"0-40%" :"Lacking work life balance", "40%-80%":"Approaching burnout due to huge workload", "80%-100%":"Action required"},
-             "not_burned_out_green":{"0-100%":"All going good"},
+             "not_burned_out_green": {"0-100%":"All going good"},
              "burned_out_red" : {"0-40%":"Proper planning of work required", "40%-80%":"Redistribution of work by manager", "80%-100%":"HR action required"}
             }
