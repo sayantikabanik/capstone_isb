297087521a2b7c9eac459fa3984ff245ae7280c5
sayantikabanik <sayantikabanik122@gmail.com>
2022-07-27 20:05:50 +0530

    dashboard updated

--- analysis_framework/pipeline/process.py
+++ analysis_framework/pipeline/process.py
@@ -1,7 +1,7 @@
 import intake
 import pandas as pd
 
-from dagster import get_dagster_logger, job, op, Output, graph
+from dagster import get_dagster_logger, job, op, Output, graph, Out
 from analysis_framework import DATA_CATALOG_PATH, PIPELINE_PATH
 from factor_analyzer import FactorAnalyzer
 
@@ -39,9 +39,9 @@ def factor_analysis(df_subset_fa) -> Output:
     fa = FactorAnalyzer(n_factors=3, rotation='oblimin')
     fa.fit(df_subset_fa)
     df_factor_scores = pd.DataFrame(fa.transform(df_subset_fa))
-    df_subset_fa[['exhaustion', 'Depersonalization', 'PersonalAccomplishment']] = df_factor_scores
+    df_subset_fa[['exhaustion', 'Depersonalization',
+                  'PersonalAccomplishment']] = df_factor_scores
     fa_data = df_subset_fa
-    print(fa_data)
     return fa_data
 
 
---
+++ analysis_framework/utils/static.py
@@ -0,0 +1,19 @@
+# static variables
+
+amber = {"0-40": "Lacking work life balance",
+         "40-80": "Approaching burnout due to huge workload",
+         "80-100": "Action required"}
+
+red = {"0-40": "Proper planning of work required",
+       "40-80": "Redistribution of work by manager",
+       "80-100": "HR action required"}
+
+locations_dropdown = ['Lucknow', 'Noida', 'Chennai', 'Pune',
+                      'Bengaluru', 'Mumbai', 'Hyderabad', 'Madurai',
+                      'Nagpur', 'New Delhi', 'Gurgaon', 'Coimbatore',
+                      'Kolkata', 'Jaipur', 'Ahmedabad', 'Thane',
+                      'Chandigarh', 'Kochi', 'Indore', 'Trivandrum',
+                      'Mangalore',  'Mangalore', 'Gandhinagar', 'Mysore',
+                      'Bhubaneshwar', 'Others']
+
+company_dropdown = ['HCL Technologies', 'Infosys', 'TCS', 'Wipro']
