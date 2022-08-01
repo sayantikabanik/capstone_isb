4d0dc7fac84430f3036986fa2590e05bcb5103b2
AnizD <63470690+AnizD@users.noreply.github.com>
2022-08-01 21:43:11 +0530

    Add files via upload
    HBR Recommendations

---
+++ analysis_framework/utils/Reco.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+amber = {"0-40": "Harm-minimization strategies to pace oneself rather than allowing obligations to consume us - compartmentalize, use small periods of time for mental rest or physical decompression techniques.",
+         "40-80": "Stress is a happiness killer. Practice self-care, altering perspective can buffer the negative impacts of even inflexible aspects of work, reset expectations with colleagues, clients, seek rich interpersonal interactions.",
+         "80-100": "In busy organizational lives, ‘permission not to do’ needs to be negotiated. We don’t have the capacity to ‘do it all’. Moments of ‘not doing’ can spur creativity and productivity. Allow structured sabbaticals to undergo some program - renewed passion helps to combat burnout."}
+
+red = {"0-40": "Human brain can focus for around 90-120 minutes then it needs rest. The need for regular breaks, calendar invites to take short breaks, encouraging private workspaces to cut down distractions, for teams across different geographies setting boundaries around time, flexible work options. Poorly run meetings also can be the biggest time sinks. Meeting hygiene to be followed like having an agenda, limited time, concrete action plan, having an email blackout.",
+       "40-80": "Stress makes people nearly three times as likely to leave their jobs, temporarily impairs strategic thinking and dulls creative abilities. Increasing psychological safety by giving clear goals, ensuring that everyone’s voices are heard, building a work environment that is challenging but not threatening. Ensuring good practices in place for gender equality.",
+       "80-100": "Make health a priority as an Organization, tackle the problem as a group, make downtime a priority, non-negotiable time off, peer coaching, develop a shared sense of ‘why’. Make rules to combat loneliness for employees like coming to the office once a week etc (hybrid model). Executive maturity – a shift from ego-drive to co-drive. Leadership showing empathy lack of which leads to depersonalization and emotional exhaustion."}
+
+locations_dropdown = ['Lucknow', 'Noida', 'Chennai', 'Pune',
+                      'Bengaluru', 'Mumbai', 'Hyderabad', 'Madurai',
+                      'Nagpur', 'New Delhi', 'Gurgaon', 'Coimbatore',
+                      'Kolkata', 'Jaipur', 'Ahmedabad', 'Thane',
+                      'Chandigarh', 'Kochi', 'Indore', 'Trivandrum',
+                      'Mangalore',  'Mangalore', 'Gandhinagar', 'Mysore',
+                      'Bhubaneshwar', 'Others']
+
+company_dropdown = ['HCL Technologies', 'Infosys', 'TCS', 'Wipro']
+
