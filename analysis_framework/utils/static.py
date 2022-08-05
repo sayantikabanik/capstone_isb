01ce9d01d2c2ef420b4e33a8574234a4a4f46e8b
sayantikabanik <sayantikabanik122@gmail.com>
2022-08-05 08:39:20 +0530

    minor fixes and comments

--- analysis_framework/utils/static.py
+++ analysis_framework/utils/static.py
@@ -1,5 +1,7 @@
 # static variables
 
+# Recommendation
+# -----------------
 amber = {"0-40": "Lacking work life balance",
          "40-80": "Approaching burnout due to huge workload",
          "80-100": "Action required"}
@@ -8,6 +10,8 @@ red = {"0-40": "Proper planning of work required",
        "40-80": "Redistribution of work by manager",
        "80-100": "HR action required"}
 
+# Location dropdown for dashboard
+# ---------------------------------
 locations_dropdown = ['Lucknow', 'Noida', 'Chennai', 'Pune',
                       'Bengaluru', 'Mumbai', 'Hyderabad', 'Madurai',
                       'Nagpur', 'New Delhi', 'Gurgaon', 'Coimbatore',
@@ -16,8 +20,17 @@ locations_dropdown = ['Lucknow', 'Noida', 'Chennai', 'Pune',
                       'Mangalore',  'Mangalore', 'Gandhinagar', 'Mysore',
                       'Bhubaneshwar', 'Others']
 
+# Company dropdown for dashboard
+# ---------------------------------
 company_dropdown = ['HCL Technologies', 'Infosys', 'TCS', 'Wipro']
 
-bonus_words_list=['work life balance', 'job security', 'work environment', 'career growth', 'salary', 'culture', 'career' ,'pay', 'improvement', 'manager', 'employees', 'company', 'compensation', 'appraisal']
-
-stigma_words_list = ['worst', 'low', 'poor', 'bad', 'insecurity', 'no', 'long', 'slow', 'less', 'pressure', 'struggle', 'lack', 'decent', 'issues', 'exhaustion', 'detachment', 'boring', 'hectic', 'pathetic', 'challenging']
+# Text summarization
+# ---------------------------------
+bonus_words_list = ['work life balance', 'job security', 'work environment',
+                    'career growth', 'salary', 'culture', 'career' , 'pay',
+                    'improvement', 'manager', 'employees', 'company',
+                    'compensation', 'appraisal']
+stigma_words_list = ['worst', 'low', 'poor', 'bad', 'insecurity', 'no',
+                     'long', 'slow', 'less', 'pressure', 'struggle', 'lack',
+                     'decent', 'issues', 'exhaustion', 'detachment',
+                     'boring', 'hectic', 'pathetic', 'challenging']
