590bd5a6bbe4e0424dd75ecf3579a2466803816c
sayantikabanik <sayantikabanik122@gmail.com>
2022-08-05 09:13:00 +0530

    tweaks

--- analysis_framework/utils/static.py
+++ analysis_framework/utils/static.py
@@ -2,13 +2,39 @@
 
 # Recommendation
 # -----------------
-amber = {"0-40": "Lacking work life balance",
-         "40-80": "Approaching burnout due to huge workload",
-         "80-100": "Action required"}
+amber = {"0-40": "Harm-minimization strategies to pace oneself rather than allowing obligations to "
+                 "consume us - compartmentalize, use small periods of time for mental rest "
+                 "or physical decompression techniques.",
+         "40-80": "Stress is a happiness killer. Practice self-care, altering perspective can buffer"
+                  " the negative impacts of even inflexible aspects of work, reset expectations "
+                  "with colleagues, clients, seek rich interpersonal interactions.",
+         "80-100": "In busy organizational lives, ‘permission not to do’ needs to be "
+                   "negotiated. We don’t have the capacity to ‘do it all’. Moments of"
+                   " ‘not doing’ can spur creativity and productivity. Allow structured "
+                   "sabbaticals to undergo some program - renewed passion helps to combat burnout."}
 
-red = {"0-40": "Proper planning of work required",
-       "40-80": "Redistribution of work by manager",
-       "80-100": "HR action required"}
+red = {"0-40": "Human brain can focus for around 90-120 minutes then it needs rest. "
+               "The need for regular breaks, calendar invites to take short breaks, "
+               "encouraging private workspaces to cut down distractions, for teams "
+               "across different geographies setting boundaries around time, "
+               "flexible work options. Poorly run meetings also can be the "
+               "biggest time sinks. Meeting hygiene to be followed like "
+               "having an agenda, limited time, concrete action plan, "
+               "having an email blackout.",
+       "40-80": "Stress makes people nearly three times as likely to leave their jobs, "
+                "temporarily impairs strategic thinking and dulls creative abilities. "
+                "Increasing psychological safety by giving clear goals, ensuring that "
+                "everyone’s voices are heard, building a work environment that is "
+                "challenging but not threatening. Ensuring good practices in place "
+                "for gender equality.",
+       "80-100": "Make health a priority as an Organization, tackle the problem as "
+                 "a group, make downtime a priority, non-negotiable time off, peer "
+                 "coaching, develop a shared sense of ‘why’. Make rules to combat "
+                 "loneliness for employees like coming to the office once a week etc"
+                 " (hybrid model). Executive maturity – a shift from ego-drive to co-drive. "
+                 "Leadership showing empathy lack of which leads to depersonalization and "
+                 "emotional exhaustion."}
+
 
 # Location dropdown for dashboard
 # ---------------------------------
